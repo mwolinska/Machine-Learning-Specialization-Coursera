face2cca8b3322ea9c64ee5b4069bfc075b9bba0
Marta <mswolinska@gmail.com>
2022-08-10 17:13:02 +0100

    Update prepare dataset function to return 3 datasets

--- regression/dataset_processing/dataset_utils.py
+++ regression/dataset_processing/dataset_utils.py
@@ -25,3 +25,22 @@ def prepare_datasets(
     # TODO(marta): create interface for datasets
     full_dataset_array = create_array_from_csv_file(filename, delimiter=delimiter)
     full_dataset = Dataset.from_array(full_dataset_array)
+    # split dataset into train, validate, test datasets
+    dataset_size = full_dataset.feature_data.shape[0]
+    test_set_ratio = (1 - training_set_ratio) / 2
+    split_index_train_dataset = int(dataset_size * training_set_ratio)
+    split_index_test_dataset = split_index_train_dataset + int(dataset_size * test_set_ratio)
+
+    training_dataset = Dataset.from_dataset(
+        base_dataset=full_dataset,
+        feature_row_tuple=(0, split_index_train_dataset),
+    )
+    test_dataset = Dataset.from_dataset(
+        base_dataset=full_dataset,
+        feature_row_tuple=(split_index_train_dataset, split_index_test_dataset),
+    )
+    validate_dataset = Dataset.from_dataset(
+        base_dataset=full_dataset,
+        feature_row_tuple=(split_index_test_dataset, None),
+    )
+    return training_dataset, validate_dataset, test_dataset
