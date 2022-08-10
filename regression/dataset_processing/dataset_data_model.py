269459efebe79a67c25ff77f5a9f7cbc8ffa91d8
Marta <mswolinska@gmail.com>
2022-08-10 17:12:34 +0100

    Create from_dataset method for Dataset dataclass

--- regression/dataset_processing/dataset_data_model.py
+++ regression/dataset_processing/dataset_data_model.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import List
+from typing import List, Tuple, Optional
 
 import numpy as np
 
@@ -22,4 +22,26 @@ class Dataset:
             feature_names=feature_names,
             label_data=label_data,
             label_name=label_name,
+        )
+
+    @classmethod
+    def from_dataset(
+        cls,
+        base_dataset: "Dataset",
+        feature_row_tuple: Tuple[Optional[int], Optional[int]],
+        feature_column_tuple: Tuple[Optional[int], Optional[int]] = (None, None),
+    ) -> "Dataset":
+        new_feature_data = base_dataset.feature_data[
+            feature_row_tuple[0]:feature_row_tuple[1],
+            feature_column_tuple[0]:feature_column_tuple[1]
+        ]
+        new_label_data = base_dataset.label_data[feature_row_tuple[0]:feature_row_tuple[1]]
+
+        new_feature_names = base_dataset.feature_names[feature_column_tuple[0]:feature_column_tuple[1]]
+
+        return cls(
+            feature_data=new_feature_data,
+            feature_names=new_feature_names,
+            label_data=new_label_data,
+            label_name=base_dataset.label_name,
         )
