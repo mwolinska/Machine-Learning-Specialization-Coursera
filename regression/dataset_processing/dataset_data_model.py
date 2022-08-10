4d99aa0bb20af06e54265af9309b7f5ef51970be
Marta <mswolinska@gmail.com>
2022-08-10 17:06:53 +0100

    Add from_array class method to Dataset

--- regression/dataset_processing/dataset_data_model.py
+++ regression/dataset_processing/dataset_data_model.py
@@ -10,3 +10,16 @@ class Dataset:
     feature_names: List
     label_data: np.ndarray
     label_name: str
+
+    @classmethod
+    def from_array(cls, dataset_array: np.ndarray) -> "Dataset":
+        label_name = dataset_array[0][-1]
+        feature_names = list(dataset_array[0][:-1])
+        label_data = dataset_array[1:, -1].astype(float)
+        feature_data = dataset_array[1:, :-1].astype(float)
+        return cls(
+            feature_data=feature_data,
+            feature_names=feature_names,
+            label_data=label_data,
+            label_name=label_name,
+        )
